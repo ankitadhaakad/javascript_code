4276a3ef2c5536f1c27580fba4ffde7951bafe76
Ankita Dhakad <ankudhakad444@gmail.com>
2025-11-15 22:34:02 +0530

    updated function.js

--- function.js
+++ function.js
@@ -60,3 +60,39 @@ arr.forEach((val) =>{
     console.log(val)
 })
     
+let num =[71,52,83,64,55]
+num.forEach((nums)=>{
+    console.log(nums*nums)
+})
+
+let nums =[10,20,30]
+let newArray=nums.map((val)=>{// map return new array
+    return val * 2;
+  //  console.log(val);
+})
+console.log(newArray)
+
+let arr2=[2,3,4,5,6,7,8]
+let newSum=arr2.filter((val)=>{
+    return val % 2 ===0
+})
+console.log(newSum)
+
+let arr3=[4,7,9,6,5,3]
+const newArr3=arr3.reduce((curr,res)=>{
+    return curr +res;
+})
+console.log(newArr3);
+
+let arr5=[9,12,8,7,6,5]
+const output =arr5.reduce((prev,curr)=>{
+    return prev>curr ? prev :curr;//largest value print
+})
+console.log(output)
+
+let n =prompt("enter a number: ")
+let arr6=[];
+for(let i=1;i<=n;i++){
+    arr6[i-1]=i
+}
+console.log(arr6)
